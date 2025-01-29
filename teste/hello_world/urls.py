d7642ae930aa8c2aee49dd012bd10f2cb3d4707a
Kelwin Ladeira <ladeirakelwin@gmail.com>
2025-01-29 17:49:12 -0300

    creating helloWorld urls

--- teste/hello_world/urls.py
+++ teste/hello_world/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from . import views
 
 app_name = 'hello_world'
 
 urlpatterns = [
+    path('', views.HelloWorldView.as_view(), name='hello')
 ]
