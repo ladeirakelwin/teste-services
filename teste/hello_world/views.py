d02a51820d1c7f38e2bc659aa66ded452ec4c5d5
Kelwin Ladeira <ladeirakelwin@gmail.com>
2025-01-29 17:48:22 -0300

    creating HelloWordView

--- teste/hello_world/views.py
+++ teste/hello_world/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+import json
+
+class HelloWorldView(View):
+    def get(self, request, *args, **kwargs):
+        msg = "Hello World!"
+        return HttpResponse(msg)
 
 # Create your views here.
